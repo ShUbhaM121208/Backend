25a94d7cfb66f310afaf9084fd738e855b7e81b4
ShUbhaM121208 <157300442+ShUbhaM121208@users.noreply.github.com>
2025-03-02 21:23:43 +0530

    Delete src directory

--- src/utils.ts
+++
@@ -1,7 +0,0 @@
-import { Client } from 'pg';
-
-export async function getClient() {
-    const client = new Client("postgresql://db_owner:npg_1VkoUFYvG0fJ@ep-still-snow-a8co0oms-pooler.eastus2.azure.neon.tech/db?sslmode=require");
-    await client.connect();
-    return client;
-}
